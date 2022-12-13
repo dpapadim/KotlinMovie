e0707d147cde20f7e6631ec4916f505477695eba
Dimitris <papajimd1994@gmail.com>
2022-12-13 17:24:36 +0200

    AndroidMovieAppInKotlin

---
+++ app/src/main/java/com/example/movieappinkotlin/Api.kt
@@ -0,0 +1,23 @@
+package com.example.movieappinkotlin
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api { @GET("movie/popular")
+
+fun getPopularMovies(
+    @Query("api_key") apiKey: String = "28cd7bccfa303b2cc9ffb11d4a0b4c22",
+    @Query("page") page: Int
+): Call<GetMoviesResponse>
+
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+        @Query("api_key") apiKey: String = "YOUR_API_KEY_HERE",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+}
+
+
---
+++ app/src/main/java/com/example/movieappinkotlin/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.movieappinkotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var popularMovies: RecyclerView
+    private lateinit var popularMoviesAdapter: MoviesAdapter
+
+    private fun showMovieDetails(movie: Movie) {
+
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        intent.putExtra(MOVIE_BACKDROP, movie.backdropPath)
+        intent.putExtra(MOVIE_POSTER, movie.posterPath)
+        intent.putExtra(MOVIE_TITLE, movie.title)
+        intent.putExtra(MOVIE_RATING, movie.rating)
+        intent.putExtra(MOVIE_RELEASE_DATE, movie.releaseDate)
+        intent.putExtra(MOVIE_OVERVIEW, movie.overview)
+        startActivity(intent)
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        popularMovies = findViewById(R.id.popular_movies)
+        popularMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+
+        )
+
+        popularMoviesAdapter = MoviesAdapter(mutableListOf()) { movie -> showMovieDetails(movie) }
+        popularMovies.adapter = popularMoviesAdapter
+
+        MoviesRepository.getPopularMovies(
+            onSuccess = ::onPopularMoviesFetched,
+            onError = ::onError
+        )
+
+    }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        popularMoviesAdapter.updateMovies(movies)
+    }
+
+    private fun onError() {
+        Toast.makeText(this, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+    }
+
+
+}
